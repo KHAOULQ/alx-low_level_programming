cc0f03ac916c327d47d9f0dfeb42080b970814c0
khaoula <khaoula.zouine@uit.ac.ma>
2023-06-20 12:37:27 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - first 0putshar for number
+ *
+ * Return: code verfier 0.
+ */
+int main(void)
+{
+char c[] = "_putchar";
+int i;
+for (i = 0; i < 8; i++)
+{
+putchar(c[i]);
+}
+putchar('\n');
+return (0);
+}
+
