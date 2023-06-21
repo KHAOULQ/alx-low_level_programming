2008dae683cb4e7b528451da1440577c401a6172
khaoula <khaoula.zouine@uit.ac.ma>
2023-06-21 14:00:17 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - prints 10 times the alphabe.
- * Return : Always return 0
+ * print_alphabet_x10 - prints .
+ * Return : Always return 0.
  */
 void print_alphabet_x10(void)
 {
