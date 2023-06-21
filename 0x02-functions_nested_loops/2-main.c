c5805bfadc24296a0bd558b79381206c55c963fe
khaoula <khaoula.zouine@uit.ac.ma>
2023-06-21 14:40:53 -0700

    task 2

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - check the code.
+ * main - check the code.
  * Return: Always 0.
  *
  */
-void print_alphabet_x10(void)
+int main(void)
 {
     print_alphabet_x10();
     return(0);
