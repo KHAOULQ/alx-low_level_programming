9fc53cc2945f09982397834dca3615e6f034ee8d
khaoula <khaoula.zouine@uit.ac.ma>
2023-06-23 14:13:00 -0700

    a function that checks for a digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,17 +1,16 @@
 #include "main.h"
+
+
 /**
- * _isdigit - Checks ^^ for digit character.
- * @c: The character to check
+ * _isupper - checks Write a function that checks for uppercase character.
+ * @c: character to check
  *
- * Return: 1 if  a digit.
- * Otherwise, 0 IF returned.
+ * Return: gives 1 or 0
  */
-
-
-int _isdigit(int c)
+int _isupper(int c)
 {
-	if (c >= 48 && c <= 57)
-		return (1);
+        if (c >= 48 && c <= 57)
+                return (1);
 
-	return (0);
+        return (0);
 }
